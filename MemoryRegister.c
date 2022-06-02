0999813ad423ec3b69b9d127d5897c685c455db6
KIMEUNBI01 <72555010+KIMEUNBI01@users.noreply.github.com>
2022-06-03 01:22:08 +0900

    Update MemoryRegister.c

--- MemoryRegister.c
+++ MemoryRegister.c
@@ -101,7 +101,7 @@ unsigned int MemAccess(unsigned int A, unsigned int V, int nRW, int S) {
 void ViewRegister(void) {
     char* r[32] = { "r0","at","v0","v1","a0","a1","a2","a3","t0","t1","t2","t3","t4","t5","t6","t7","s0","s1","s2","s3","s4","s5","s6","s7","t8","t9","k0","k1","gp","sp","fp","ra" };
 
-    printf("[REGISTER[\n");
+    printf("[REGISTER]\n");
 
     for (int i = 0; i < 32; i++)
         printf("R%02d [%s] = %X\n", i, r[i], reg[i]);
