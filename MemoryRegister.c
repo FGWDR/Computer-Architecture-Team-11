6ece6b006b04670fa5707cb4c99c4dc56777dcd1
KIMEUNBI01 <72555010+KIMEUNBI01@users.noreply.github.com>
2022-06-03 01:25:11 +0900

    Update MemoryRegister.c

--- MemoryRegister.c
+++ MemoryRegister.c
@@ -97,8 +97,8 @@ unsigned int MemAccess(unsigned int A, unsigned int V, int nRW, int S) {
     return 0;
 }
 
-//레지스터 View
-void ViewRegister(void) {
+//레지스터 Print
+void PrintRegister(void) {
     char* r[32] = { "r0","at","v0","v1","a0","a1","a2","a3","t0","t1","t2","t3","t4","t5","t6","t7","s0","s1","s2","s3","s4","s5","s6","s7","t8","t9","k0","k1","gp","sp","fp","ra" };
 
     printf("[REGISTER]\n");
@@ -114,8 +114,8 @@ void ViewRegister(void) {
     fflush(stdout);
 }
 
-//메모리 View
-void ViewMemory(unsigned int start, unsigned int end) {
+//메모리 Print
+void PrintMemory(unsigned int start, unsigned int end) {
     unsigned int front, offset, e_offset;
     unsigned char* pM;
     front = start >> 20; //앞 12비트
